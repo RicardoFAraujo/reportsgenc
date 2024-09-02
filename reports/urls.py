a42868281aeec0c74b49fb948421b1878e11ad01
Ricardo <ricardo.fa@gmail.com>
2024-09-02 13:41:12 -0300

    "adição do header"

--- reports/urls.py
+++ reports/urls.py
@@ -3,9 +3,12 @@
 from django.urls import path
 from reports.views import home, report_list, report_detail
 from . import views
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('reports/', views.report_list, name='report_list'),
     path('reports/<int:report_id>/', views.report_detail, name='report_detail'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    
 ]
