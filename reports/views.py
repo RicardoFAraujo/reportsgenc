799d456d9847853b89beb8ece081a0bb950abdb6
Ricardo <ricardo.fa@gmail.com>
2024-09-02 13:06:55 -0300

    feat: Adicionar estilos ao formulário de login e lista de relatórios
    Os estilos foram adicionados ao formulário de login e à lista de relatórios para melhorar a aparência e a usabilidade das páginas. Além disso, os relatórios foram agrupados por empresa na lista de relatórios.

--- reports/views.py
+++ reports/views.py
@@ -4,21 +4,28 @@ from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from .models import Report
 from collections import defaultdict
+from itertools import groupby
+from operator import attrgetter
 def home(request):
     return render(request, 'home.html')
 
 @login_required
 def report_list(request):
     if request.user.is_superuser:  # Verifica se o usuário é um superusuário
-        reports = Report.objects.all()  # Exibe todos os relatórios
+        reports = Report.objects.all().order_by('company__name')  # Ordena por empresa
     else:
-        # Verifica se o usuário tem um perfil e se está associado a uma empresa
         if hasattr(request.user, 'userprofile') and request.user.userprofile.company:
-            reports = Report.objects.filter(company=request.user.userprofile.company)  # Exibe apenas os relatórios da empresa do usuário
+            reports = Report.objects.filter(company=request.user.userprofile.company).order_by('company__name')
         else:
-            reports = Report.objects.none()  # Se não tiver perfil ou empresa, não mostra nada
+            reports = Report.objects.none()
 
-    return render(request, 'reports/report_list.html', {'reports': reports})
+    # Agora, vamos agrupar os relatórios por empresa
+    reports_by_company = {}
+    for company, group in groupby(reports, key=attrgetter('company')):
+        reports_by_company[company] = list(group)
+    
+    print(f"Reports by company: {reports_by_company}")
+    return render(request, 'reports/report_list.html', {'reports_by_company': reports_by_company})
 
 @login_required
 def report_detail(request, report_id):
